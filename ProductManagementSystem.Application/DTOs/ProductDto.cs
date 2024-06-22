aa54fc228dd7fb0118273474fe0ed2f63dfcc34f
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2024-06-22 11:55:06 +0600

    feat:Added Dtos

---
+++ ProductManagementSystem.Application/DTOs/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.Application.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public ICollection<VariantDto> Variants { get; set; }
+
+    }
+}
---
+++ ProductManagementSystem.Application/DTOs/VariantDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.Application.DTOs
+{
+    public class VariantDto
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public string Specification { get; set; }
+        public string Size { get; set; }
+    }
+}
