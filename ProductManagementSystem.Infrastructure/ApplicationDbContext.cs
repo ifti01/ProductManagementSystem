c4fb818146630dc764d3f68661ca18824beb87b1
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2024-06-22 10:50:42 +0600

    feat:Applicationdbcontext added

---
+++ ProductManagementSystem.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementSystem.Domain.Entities;
+
+namespace ProductManagementSystem.Infrastructure
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
