abcaa2104d311aff876f40f46bf9a965491f384d
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2024-06-22 17:56:43 +0600

    feat:endpoint added

---
+++ ProductManagementSystem.Api/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagementSystem.Application.DTOs;
+using ProductManagementSystem.Application.Interfaces;
+
+namespace ProductManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAll()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetById(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ProductDto productDto)
+        {
+            await _productService.CreateProductAsync(productDto);
+            return CreatedAtAction(nameof(GetById), new { id = productDto.Id }, productDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, ProductDto productDto)
+        {
+            if (id != productDto.Id)
+            {
+                return BadRequest();
+            }
+            await _productService.UpdateProductAsync(productDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _productService.DeleteProductAsync(id);
+            return NoContent();
+        }
+    }
+}
