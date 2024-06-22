d7cc6de3bb8c0136f8cd1e64e6314d67c2c4a065
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2024-06-22 11:55:30 +0600

    feat:added interface

---
+++ ProductManagementSystem.Application/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductManagementSystem.Application.Interfaces
+{
+    public class IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task<ProductDto> GetProductByIdAsync(int id);
+        Task CreateProductAsync(ProductDto productDto);
+        Task UpdateProductAsync(ProductDto productDto);
+        Task DeleteProductAsync(int id);
+    }
+}
