1901b51c0ba9ca8922340e1335d09edda91b316e
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2024-06-22 10:42:34 +0600

    feat:Added Userinfo Class

--- ProductManagementSystem.Domain/Entities/UserInfo.cs
+++ ProductManagementSystem.Domain/Entities/UserInfo.cs
@@ -10,6 +10,7 @@ namespace ProductManagementSystem.Domain.Entities
     {
         public string? Name { get; set; }
         public string? Email { get; set; }
+
         public string? Address { get; set; }
     }
 }
