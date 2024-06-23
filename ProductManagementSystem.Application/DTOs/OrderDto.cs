4b945ebda58519be0ead63cc19924b70c08a8ad9
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2024-06-23 11:22:46 +0600

    feat:dto added

---
+++ ProductManagementSystem.Application/DTOs/OrderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.Application.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public ICollection<OrderItemDto> OrderItems { get; set; }
+        public UserInfoDto UserInfo { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ ProductManagementSystem.Application/DTOs/UserInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.Application.DTOs
+{
+    public class UserInfoDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
